3411aaf8468549143d67524fa0fdb3d20f623f65
Raghavendra.Bn <Raghavendra.Bn@target.com>
2017-09-10 11:41:15 +0530

    changes in font size

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,17 +5,17 @@ const Header = () => {
         <div>
         <h1>
             <br/>
-            <span className="color-grayDarkest fnt-h1">Hotels.com - </span>
-            <span className = "color-gray fnt-h2">wake up happy</span>
+            <span className="color-grayDarkest ft-h1">Hotels.com - </span>
+            <span className = "color-gray ft-h2">wake up happy</span>
         </h1>
         <br/>
         <p>
             <span className = "color-grayDark ft-lg">With over </span>
-            <span className = "color-grayDark fnt-lg bold">140,000 hotels </span>
-            <span className = "color-grayDark fnt-lg">worldwide, </span>
-            <span className = "color-grayDark fnt-lg bold">price matched guaranteed </span>
-            <span className = "color-grayDark fnt-lg">and </span>
-            <span className = "color-grayDark fnt-lg bold">1.2 million </span>
+            <span className = "color-grayDark ft-lg bold">140,000 hotels </span>
+            <span className = "color-grayDark ft-lg">worldwide, </span>
+            <span className = "color-grayDark ft-lg bold">price matched guaranteed </span>
+            <span className = "color-grayDark ft-lg">and </span>
+            <span className = "color-grayDark ft-lg bold">1.2 million </span>
             <span className = "color-grayDark ft-lg">reviews, why look elsewhere for your next hotel booking.</span>
         </p>
         </div>
